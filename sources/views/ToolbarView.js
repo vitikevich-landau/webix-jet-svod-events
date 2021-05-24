dec91b5caadda2551e80d34233ac0c5af65afacc
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2021-05-24 12:02:25 +0700

    # Add information.

--- sources/views/ToolbarView.js
+++ sources/views/ToolbarView.js
@@ -8,8 +8,27 @@ export default class ToolbarView extends JetView {
 			id: "toolbar",
 			// height: 40,
 			cols: [
-				{template: "Заявки СВОД-СМАРТ", type: "header", borderless: true},
+				{
+					width: 250,
+					template: "Заявки СВОД-СМАРТ",
+					type: "header",
+					borderless: true
+				},
+				{
+					width: 620,
+					template: "Горячая линия «СВОДЫ» 8(38822) 4-70-61 добавочный 300, 8-913-690-33-47",
+					type: "header",
+					css: {"font-size": "90%"},
+					borderless: true
+				},
 				{},
+				{
+					width: 180,
+					template: `<a href='https://forms.parusaltai.ru:8185' target='_blank' style="color: darkred">Задать вопрос</a>`,
+					type: "header",
+					css: {"text-align": "right"},
+					borderless: true
+				},
 				RecordCounterView
 			]
 		};
