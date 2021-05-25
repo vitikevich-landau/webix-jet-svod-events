6b8ded6269baed737073ae187ebef745654cc931
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2021-05-25 09:12:09 +0700

    # Перенесена логика загрузки и отображения прелоадера из коллекции во View -> Datatable.

--- sources/models/EventsCollection.js
+++ sources/models/EventsCollection.js
@@ -1,50 +1,64 @@
-import {SERVER_URI} from "../config";
-import Utils from "./Utils";
-
-class EventsCollection {
-	constructor() {
-		this._data = new webix.DataCollection({
-			on: {
-				onBeforeLoad: function () {
-					webix.extend($$("events"), webix.ProgressBar);
-
-					$$("events").showProgress();
-				},
-				onAfterLoad: function () {
-					if ($$("events").count()) {
-						$$("events").hideProgress();
-					} else {
-						$$("events").hideProgress();
-						$$("events").showOverlay("Нет данных");
-					}
-				}
-			}
-		});
-	}
-
-	refresh() {
-		this._data.clearAll();
-		return this._data.load({
-			$proxy: true,
-			load: (view, params) => {
-				return webix.ajax()
-					.get(SERVER_URI, params)
-					.then(response => response.json())
-					.then(
-						json => json.map(v => ({
-							...v,
-							REG_DATE: Utils.parseStrDate(v.REG_DATE),
-							PLAN_DATE: Utils.parseStrDate(v.PLAN_DATE),
-							SROK: Utils.parseStrDate(v.SROK)
-						}))
-					);
-			}
-		});
-	}
-
-	get data() {
-		return this._data;
-	}
-}
-
-export const eventsCollection = new EventsCollection();
+// import {SERVER_URI} from "../config";
+// import Utils from "./Utils";
+//
+// class EventsCollection {
+// 	constructor() {
+// 		this._data = new webix.DataCollection({
+// 			// url: (view, params) => {
+// 			// 	return webix.ajax()
+// 			// 		.get(SERVER_URI, params)
+// 			// 		.then(response => response.json())
+// 			// 		.then(
+// 			// 			json => json.map(v => ({
+// 			// 				...v,
+// 			// 				REG_DATE: Utils.parseStrDate(v.REG_DATE),
+// 			// 				PLAN_DATE: Utils.parseStrDate(v.PLAN_DATE),
+// 			// 				SROK: Utils.parseStrDate(v.SROK)
+// 			// 			}))
+// 			// 		);
+// 			// },
+// 			// on: {
+// 				// onBeforeLoad: function () {
+// 				// 	// webix.extend($$("events"), webix.ProgressBar);
+// 				// 	console.log($$("events"));
+// 				//
+// 				// 	// $$("events").showProgress();
+// 				// },
+// 				// onAfterLoad: function () {
+// 				// 	if ($$("events").count()) {
+// 				// 		$$("events").hideProgress();
+// 				// 	} else {
+// 				// 		$$("events").hideProgress();
+// 				// 		$$("events").showOverlay("Нет данных");
+// 				// 	}
+// 				// }
+// 			// }
+// 		});
+// 	}
+//
+// 	// refresh() {
+// 	// 	this._data.clearAll();
+// 	// 	return this._data.load({
+// 	// 		$proxy: true,
+// 	// 		load: (view, params) => {
+// 	// 			return webix.ajax()
+// 	// 				.get(SERVER_URI, params)
+// 	// 				.then(response => response.json())
+// 	// 				.then(
+// 	// 					json => json.map(v => ({
+// 	// 						...v,
+// 	// 						REG_DATE: Utils.parseStrDate(v.REG_DATE),
+// 	// 						PLAN_DATE: Utils.parseStrDate(v.PLAN_DATE),
+// 	// 						SROK: Utils.parseStrDate(v.SROK)
+// 	// 					}))
+// 	// 				);
+// 	// 		}
+// 	// 	});
+// 	// }
+//
+// 	get data() {
+// 		return this._data;
+// 	}
+// }
+//
+// export const eventsCollection = new EventsCollection();
--- sources/views/EventsView.js
+++ sources/views/EventsView.js
@@ -1,15 +1,30 @@
 import {JetView} from "webix-jet";
-import {eventsCollection} from "../models/EventsCollection";
 import {eventsDescriptionCollection} from "../models/EventDescriptionCollection";
 import {eventsDetailsCollection} from "../models/EventsDetailsCollection";
 import {eventAttachmentsCollection} from "../models/EventAttachmentsCollection";
+import {SERVER_URI} from "../config";
+import Utils from "../models/Utils";
 
 export default class EventsView extends JetView {
 	config() {
+		const thisView = this;
 		const css = {"font-weight": "bold"};
 		return {
 			id: "events",
 			view: "datatable",
+			url: (view, params) => {
+				return webix.ajax()
+					.get(SERVER_URI, params)
+					.then(response => response.json())
+					.then(
+						json => json.map(v => ({
+							...v,
+							REG_DATE: Utils.parseStrDate(v.REG_DATE),
+							PLAN_DATE: Utils.parseStrDate(v.PLAN_DATE),
+							SROK: Utils.parseStrDate(v.SROK)
+						}))
+					);
+			},
 			columns: [
 				{
 					id: "ROWNUM",
@@ -29,7 +44,7 @@ export default class EventsView extends JetView {
 				},
 				{
 					id: "SDOC_PREFNUMB",
-					header: [{text: "Номер заявки", css}, {content: "textFilter"}],
+					header: [{text: "№ заявки", css}, {content: "textFilter"}],
 					adjust: "header",
 					sort: "string",
 				},
@@ -106,6 +121,19 @@ export default class EventsView extends JetView {
 			select: true,
 			fixedRowHeight: false,
 			on: {
+				onBeforeLoad: function () {
+					webix.extend(this, webix.ProgressBar);
+
+					this.showProgress();
+				},
+				onAfterLoad: function () {
+					if (this.count()) {
+						this.hideProgress();
+					} else {
+						this.hideProgress();
+						this.showOverlay("Нет данных");
+					}
+				},
 				onAfterFilter: function () {
 					const firstID = this.getFirstId();
 
@@ -123,6 +151,9 @@ export default class EventsView extends JetView {
 				},
 				onAfterSelect: function (obj, preserve) {
 					const rn = this.getItem(obj.row).RN;
+
+					thisView.setParam("rn", rn, true);
+
 					eventsDescriptionCollection.refresh(rn);
 					eventsDetailsCollection.refresh(rn)
 						.then(() => {
@@ -141,7 +172,7 @@ export default class EventsView extends JetView {
 	init(_$view, _$) {
 		super.init(_$view, _$);
 
-		_$view.parse(eventsCollection.data);
+		// _$view.parse(eventsCollection.data);
 
 		/***
 		 * 	Need timeout autoUpdate !!!
@@ -151,17 +182,51 @@ export default class EventsView extends JetView {
 	urlChange(_$view, _$url) {
 		super.urlChange(_$view, _$url);
 
-		eventsCollection.refresh().then(() => {
-			const firstID = _$view.getFirstId();
 
-			if (firstID) {
-				_$view.select(_$view.getFirstId());
+		_$view.waitData.then(() => {
+			const rn = this.getParam("rn") || _$view.getFirstId();
+
+			const item = _$view.find(obj => obj.RN === +rn, true);
+
+			if (item) {
+				if (_$view.exists(item.id)) {
+					_$view.select(item.id);
+					_$view.showItem(item.id);
+				}
+			} else {
+				if (_$view.getFirstId()) {
+					_$view.select(_$view.getFirstId());
+				}
 			}
-
-			// _$view.adjustRowHeight("NOTE", false);
-			// _$view.refresh();
 
 			$$("counter").setValues({data: `Количество записей: ${_$view.data.count()}`});
 		});
 	}
+
+	// ready(_$view, _$url) {
+	// 	super.ready(_$view, _$url);
+	//
+	// 	_$view.waitData.then(() => {
+	// 		if (_$view.getFirstId()) {
+	// 			_$view.select(_$view.getFirstId());
+	// 		}
+	// 	});
+	// }
+
+	// urlChange(_$view, _$url) {
+	// 	super.urlChange(_$view, _$url);
+	//
+	// 	eventsCollection.refresh().then(() => {
+	// 		const firstID = _$view.getFirstId();
+	//
+	// 		if (firstID) {
+	// 			_$view.select(_$view.getFirstId());
+	// 		}
+	//
+	// 		// _$view.adjustRowHeight("NOTE", false);
+	// 		// _$view.refresh();
+	//
+	// 		$$("counter").setValues({data: `Количество записей: ${_$view.data.count()}`});
+	// 	});
+	// }
 }
