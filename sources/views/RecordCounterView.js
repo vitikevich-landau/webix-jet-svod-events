bd4e056d54052744e7623d1cbced82a2af2582e0
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2021-05-25 09:10:59 +0700

    # Change width.

--- sources/views/RecordCounterView.js
+++ sources/views/RecordCounterView.js
@@ -3,7 +3,7 @@ import {JetView} from "webix-jet";
 export default class RecordCounterView extends JetView {
 	config() {
 		return {
-			width: 180,
+			width: 170,
 			view: "template",
 			id: "counter",
 			type: "header",
