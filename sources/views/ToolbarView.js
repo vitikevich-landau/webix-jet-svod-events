0c3bb459497da2c4c3bc7dad7f84c197d8003a15
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2021-05-25 09:10:07 +0700

    # Add views width.

--- sources/views/ToolbarView.js
+++ sources/views/ToolbarView.js
@@ -4,29 +4,30 @@ import RecordCounterView from "jet-views/RecordCounterView";
 export default class ToolbarView extends JetView {
 	config() {
 		return {
+			// width: 1380,
 			view: "toolbar",
 			id: "toolbar",
 			// height: 40,
 			cols: [
 				{
-					width: 250,
+					width: 200,
 					template: "Заявки СВОД-СМАРТ",
 					type: "header",
 					borderless: true
 				},
 				{
-					width: 620,
+					width: 660,
 					template: "Горячая линия «СВОДЫ» 8(38822) 4-70-61 добавочный 300, 8-913-690-33-47",
 					type: "header",
-					css: {"font-size": "90%"},
+					css: {"font-size": "90%", "font-weight": "bold"},
 					borderless: true
 				},
 				{},
 				{
-					width: 180,
+					width: 170,
 					template: `<a href='https://forms.parusaltai.ru:8185' target='_blank' style="color: darkred">Задать вопрос</a>`,
 					type: "header",
-					css: {"text-align": "right"},
+					css: {"text-align": "right", "font-weight": "bold"},
 					borderless: true
 				},
 				RecordCounterView
